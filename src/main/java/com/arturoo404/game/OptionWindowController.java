35d6e166708909f58f6b35e7e67bc50fe176a39d
Szymon Gardynik <szymongg787@gmail.com>
2023-07-14 14:44:46 +0200

    OpenOption commit v2

---
+++ src/main/java/com/arturoo404/game/OptionWindowController.java
@@ -0,0 +1,15 @@
+package com.arturoo404.game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class OptionWindowController {
+
+}
